1db4b76f2bc1f7bbcfed01b575ebb1766bdf2df4
Дмитрий Каблучко <xequrt@yandex.ru>
2024-01-09 13:21:55 +1000

    add game calc

---
+++ brain_games/games/gcd_game.py
@@ -0,0 +1,24 @@
+import random
+from random import randint
+import math
+
+RULES_GCD = 'Find the greatest common divisor of given numbers.'
+
+def game_data():
+    # Генерация случайных чисел и операторов
+    roll_number_one = randint(1, 100)
+    roll_number_two = randint(1, 100)
+
+    # Создание пары чисел для нахождения наибольшего общего делителя.
+    computer_question = f'{roll_number_one} {roll_number_two}'
+
+    # Нахождения наибольшего общего делителя пары целых чисел алгоритмом Евлклида.
+    while roll_number_one != 0 and roll_number_two != 0:
+        if roll_number_one > roll_number_two:
+            roll_number_one = roll_number_one % roll_number_two
+        else:
+            roll_number_two = roll_number_two % roll_number_one
+
+    result_game = str(roll_number_one + roll_number_two)
+
+    return computer_question, result_game
