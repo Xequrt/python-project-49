2b3b81ae8c45d0f203d8f83aeb8181ffebfcc17d
Дмитрий Каблучко <xequrt@yandex.ru>
2024-01-13 16:18:13 +1000

    add game prime

---
+++ brain_games/games/prime_game.py
@@ -0,0 +1,21 @@
+from random import randint
+
+RULES_PRIME = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+def game_data():
+    # Генерация простого числа
+    computer_question = randint(1, 100)
+    if prime_check(computer_question) == True:
+        result_game: str = "yes"
+    else:
+        result_game: str = "no"
+    return computer_question, result_game
+
+def prime_check(computer_question):
+    # Проверка числа на простоту
+    if computer_question < 2:
+        return False
+    for i in range (2, int(computer_question ** 0.5) + 1):
+        if computer_question % i == 0:
+            return False
+    return True
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from brain_games.body_game.body_game import run_game
+from brain_games.games.prime_game import RULES_PRIME, game_data
+
+
+def main():
+    run_game(RULES_PRIME, game_data)
+
+
+if __name__ == '__main__':
+    main()
