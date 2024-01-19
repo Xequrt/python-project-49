02c2c8ad6e7edb3d07266cfa7a7bd3a67edbf5c6
Дмитрий Каблучко <xequrt@yandex.ru>
2024-01-19 22:53:16 +1000

    FIX game progression list to str

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -4,18 +4,19 @@ RULES_PROGRESSION = 'What number is missing in the progression?'
 
 
 def game_data():
-    computer_question = []
+    computer_question_list = []
     # Генерация первого элемента и разности прогрессии
     roll_an = randint(1, 20)
     roll_d = randint(1, 5)
     # Генерация арифметической прогрессии в список
     for i in range(10):
-        computer_question.append(roll_an + roll_d)
+        computer_question_list.append(roll_an + roll_d)
         roll_an += roll_d
 
     # Генерация случайного индекса
-    index = randint(0, len(computer_question) - 1)
-    result_game = computer_question[index]
-    computer_question[index] = ".."
-
+    index = randint(0, len(computer_question_list) - 1)
+    result_game = computer_question_list[index]
+    computer_question_list[index] = ".."
+    computer_question_str = [str(i) for i in computer_question_list]
+    computer_question = ' '.join(computer_question_str)
     return computer_question, result_game
