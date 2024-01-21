b1db46c68742c8cb497455cb2337fa1b2428d600
Дмитрий Каблучко <xequrt@yandex.ru>
2024-01-21 12:54:19 +1000

    FIX game progression range An

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -4,17 +4,16 @@ RULES_PROGRESSION = 'What number is missing in the progression?'
 
 
 def game_data():
-    computer_question_list = []
-    # Генерация первого элемента и разности прогрессии
+    # Генерация первого элемента, разности и длины прогрессии
     roll_an = randint(1, 20)
     roll_d = randint(1, 5)
+    roll_len = randint(5, 10)
+    # Расчет конца прогрессии
+    end_an = roll_d * roll_len + roll_an
     # Генерация арифметической прогрессии в список
-    for i in range(10):
-        computer_question_list.append(roll_an + roll_d)
-        roll_an += roll_d
-
+    computer_question_list = list(range(roll_an, end_an, roll_d))
     # Генерация случайного индекса
-    index = randint(0, len(computer_question_list) - 1)
+    index = randint(0, roll_len)
     result_game = computer_question_list[index]
     computer_question_list[index] = ".."
     computer_question_str = [str(i) for i in computer_question_list]
